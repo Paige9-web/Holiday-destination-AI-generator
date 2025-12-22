2ee15ca4682ef7f6ac4f869fb884ae1fe6091977
Paige Warren <Paigewarren1@icloud.com>
2025-12-22 23:21:44 +0700

    JS and CSS update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
+new Typewriter(resultElement, {
+  strings: Response.datat.answer,
+  autoStart: true,
+  delay: 20,
+  cursor: "",
+});
+
 function generateHoliday(event) {
   event.preventDefault();
 
-  const resultElement = document.querySelector(".holiday-result");
-  resultElement.innerHTML = "";
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "470c09f57bd9o90adc47e3t0cd74b0fc";
+  let promt = `User instructions: Generate a Holiday desitnation about ${instructionsInput.value}`;
+  let context =
+    "you are a travel expert factoring in culutre, weather and activity <br> your goal is to generate a suggestion no longer than 4 lines <br> your answer must include a country, what there is to do there and the weather.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  new Typewriter(resultElement, {
-    strings: "Vietnam is very hot and the city has lots to explore!",
-    autoStart: true,
-    delay: 20,
-    cursor: "",
-  });
+  console.log("generating holiday");
 }
 
 let holidayFormElement = document.querySelector("#holiday-generator-form");
