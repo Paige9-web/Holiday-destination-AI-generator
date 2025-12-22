97b1f99e6167dbd7905c10d1c75caa28bd6b192c
Paige Warren <Paigewarren1@icloud.com>
2025-12-22 17:35:02 +0700

    JS form results

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+function generateHoliday(event) {
+  event.preventDefault();
 
+  const resultElement = document.querySelector(".holiday-result");
+  resultElement.innerHTML = "";
+
+  new Typewriter(resultElement, {
+    strings: "Vietnam is very hot and the city has lots to explore!",
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
+let holidayFormElement = document.querySelector("#holiday-generator-form");
+holidayFormElement.addEventListener("submit", generateHoliday);
